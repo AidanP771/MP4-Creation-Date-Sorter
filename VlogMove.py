36d1dc7b69814ff61647efd869dd47804d98d547
AidanP771 <aidanm771@outlook.com>
2023-03-28 02:18:46 -0400

    Update VlogMove.py
    added a function to the code that sorts the {month} {day} folders into {month} folder. It calls at the end so it sorts files into their day month folders (i.e. March 28) and then sorts all of those into Month folder (i.e. March)

--- VlogMove.py
+++ VlogMove.py
@@ -3,7 +3,8 @@ import shutil
 from datetime import datetime
 
 # Path to folder containing MP4 files
-folder_path = 'C:/Users/aidan/Desktop/VLOG/Unsorted Video'
+#folder_path = 'C:/Users/aidan/Desktop/VLOG/Unsorted Video'
+folder_path = 'C:/Users/aidan/Desktop/Test_vllog'
 
 #dict that maps month numbers to month names
 month_dict = {
@@ -20,6 +21,25 @@ month_dict = {
     11: 'November',
     12: 'December'
 }
+
+#Funtion that sorts folder into month folders
+def sort_into_month_folders():
+    #loop through folders in the folder_path
+    for folder_name in os.listdir(folder_path):
+        #check if folder name is in "Month Day" format
+        if len(folder_name.split()) == 2:
+            month_name, day_number = folder_name.split()
+            #get month number from month_dict
+            month_number = list(month_dict.keys())[list(month_dict.values()).index(month_name)]
+            #create folder for month if it doesn't already exist
+            month_folder_path = os.path.join(folder_path, month_name)
+            if not os.path.exists(month_folder_path):
+                os.mkdir(month_folder_path)
+            #move day folder into month folder
+            day_folder_path = os.path.join(folder_path, folder_name)
+            dest_folder_path = os.path.join(month_folder_path, folder_name)
+            shutil.move(day_folder_path, dest_folder_path)
+
 
 # Loop through files in folder
 for file_name in os.listdir(folder_path):
@@ -43,3 +63,6 @@ for file_name in os.listdir(folder_path):
         source_file_path = os.path.join(folder_path, file_name)
         dest_file_path = os.path.join(date_folder_path, file_name)
         shutil.move(source_file_path, dest_file_path)
+
+#call the sort_into_month_folders() function
+sort_into_month_folders()
